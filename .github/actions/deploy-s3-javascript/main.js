1f596ef1859a99d43b35ea589c9de104dd43f7a9
andrewarosario <andrew.arosario@gmail.com>
2024-03-04 15:36:31 -0300

    add deploy custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,18 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    // 1. Get the inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2. Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice('Hello from my custom JavaScript Action');
+
 }
 
 run();
